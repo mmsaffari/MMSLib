3763996fe31b693905562ce56e53fc8922d8c2d8
Mohammad Mahdi Saffari <mmsaffari@gmail.com>
2019-11-11 18:46:20 +0330

    Testing Slack #3

--- MMS.Core/TagHelpers/PageControls/PagingTagHelper.cs
+++ MMS.Core/TagHelpers/PageControls/PagingTagHelper.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+
 
 namespace MMS.Core.TagHelpers.PageControls {
 	/// <summary>
